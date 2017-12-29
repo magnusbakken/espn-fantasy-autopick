133e5d76d18da34d81940778b43ed30e2d866bf9
Magnus Grindal Bakken <magnusbakken@gmail.com>
2017-12-29 19:45:00 +0100

    Added structure of main.js.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+var resetButton = document.getElementById("pncTopResetButton");
+var autoSetupButton = document.createElement("div");
+autoSetupButton.className = "pncTopButton pncTopButtonText";
+autoSetupButton.style = "margin-left: 6px";
+autoSetupButton.textContent = "Auto";
+autoSetupButton.onclick = function() {
+    alert("Hello World!");
+}
+resetButton.parentNode.insertBefore(autoSetupButton, resetButton);
