9d97670727a82407491e884506274fad547e3a80
Magnus Grindal Bakken <magnusbakken@gmail.com>
2018-01-20 21:04:03 +0100

    Added settings page.

--- popup.js
+++ popup.js
@@ -17,43 +17,16 @@ function viewOnGitHub() {
     });
 }
 
-DEFAULT_OPTIONS = {
-    autoSave: true
-};
-
-function restoreSettings(settings) {
-    console.debug("Restoring settings", settings);
-    document.getElementById("autoSaveCheckbox").checked = settings.autoSave;
-}
-
-function saveSettings(settings) {
-    console.debug("Saving settings", settings);
-    chrome.storage.sync.set(settings, function() {
-        chrome.tabs.query({ url: "*://*.games.espn.com/fba/clubhouse*" }, function (tabs) {
-            const message = {
-                commandId: "settings-changed",
-                settings: settings,
-            };
-            for (const tab of tabs) {
-                console.debug("Sending updated settings to tab", tab);
-                chrome.tabs.sendMessage(tab.id, message);
-            }
-        })
-    });
-}
-
-function withSettings(action) {
-    chrome.storage.sync.get(DEFAULT_OPTIONS, function (items) {
-        action(items);
-    });
-}
-
 function toggleAutoSave() {
     const checkbox = document.getElementById("autoSaveCheckbox");
     withSettings(settings => {
         settings.autoSave = checkbox.checked;
         saveSettings(settings);
     });
+}
+
+function showSettingsPage() {
+    chrome.runtime.openOptionsPage();
 }
 
 function on(selector, event, action) {
@@ -69,4 +42,5 @@ document.addEventListener("DOMContentLoaded", () => {
     on("#setupCurrentPageButton", "click", performAutoSetup);
     on("#viewOnGitHubButton", "click", viewOnGitHub);
     on("#autoSaveCheckbox", "change", toggleAutoSave);
+    on("#showSettingsPageLink", "click", showSettingsPage);
 });
---
+++ settings.js
@@ -0,0 +1,83 @@
+DEFAULT_OPTIONS = {
+    autoSave: true
+};
+
+let originalSettings = DEFAULT_OPTIONS;
+
+function restoreSettings(settings) {
+    console.debug("Restoring settings", settings);
+    document.getElementById("autoSaveCheckbox").checked = settings.autoSave;
+}
+
+function saveSettings(settings) {
+    console.debug("Saving settings", settings);
+    chrome.storage.sync.set(settings, function() {
+        chrome.tabs.query({ url: "*://*.games.espn.com/fba/clubhouse*" }, function (tabs) {
+            const message = {
+                commandId: "settings-changed",
+                settings: settings,
+            };
+            for (const tab of tabs) {
+                console.debug("Sending updated settings to tab", tab);
+                chrome.tabs.sendMessage(tab.id, message);
+            }
+        })
+    });
+}
+
+function withSettings(action) {
+    chrome.storage.sync.get(DEFAULT_OPTIONS, function (items) {
+        action(items);
+    });
+}
+
+function currentSettings() {
+    return {
+        autoSave: document.getElementById("autoSaveCheckbox").checked,
+    };
+}
+
+function settingsAreEqual(settings1, settings2) {
+    for (const prop in settings1) {
+        if (settings1.hasOwnProperty(prop)) {
+            if (settings1[prop] !== settings2[prop]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function setSaveEnabled(enabled) {
+    const button = document.getElementById("saveButton");
+    if (enabled) {
+        button.removeAttribute("disabled");
+    } else {
+        button.setAttribute("disabled", "");
+    }
+}
+
+function settingsUpdated() {
+    setSaveEnabled(!settingsAreEqual(originalSettings, currentSettings()));
+}
+
+function performSave() {
+    const newSettings = currentSettings();
+    saveSettings(newSettings);
+    originalSettings = newSettings;
+    settingsUpdated();
+}
+
+function on(selector, event, action) {
+    document.querySelector(selector).addEventListener(event, () => action());
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.debug("Loading settings...");
+    withSettings(settings => {
+        restoreSettings(settings);
+        originalSettings = settings;
+    });
+    on(".setting", "change", settingsUpdated);
+    on("#saveButton", "click", performSave);
+});
