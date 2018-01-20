154ec8ecd2b33c5fadc20ef1a31c5f33f1d4dfcb
Magnus Grindal Bakken <magnusbakken@gmail.com>
2018-01-21 00:12:33 +0100

    Fixed settings page.

--- settings.js
+++ settings.js
@@ -1,36 +1,3 @@
-DEFAULT_OPTIONS = {
-    autoSave: true
-};
-
-let originalSettings = DEFAULT_OPTIONS;
-
-function restoreSettings(settings) {
-    console.debug("Restoring settings", settings);
-    document.getElementById("autoSaveCheckbox").checked = settings.autoSave;
-}
-
-function saveSettings(settings) {
-    console.debug("Saving settings", settings);
-    chrome.storage.sync.set(settings, function() {
-        chrome.tabs.query({ url: "*://*.games.espn.com/fba/clubhouse*" }, function (tabs) {
-            const message = {
-                commandId: "settings-changed",
-                settings: settings,
-            };
-            for (const tab of tabs) {
-                console.debug("Sending updated settings to tab", tab);
-                chrome.tabs.sendMessage(tab.id, message);
-            }
-        })
-    });
-}
-
-function withSettings(action) {
-    chrome.storage.sync.get(DEFAULT_OPTIONS, function (items) {
-        action(items);
-    });
-}
-
 function currentSettings() {
     return {
         autoSave: document.getElementById("autoSaveCheckbox").checked,
