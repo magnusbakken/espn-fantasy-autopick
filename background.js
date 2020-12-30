68f2b0908aab5326ccb1fe6d0815e5d004cda087
Magnus Grindal Bakken <magnusbakken@gmail.com>
2020-12-30 19:18:53 +0100

    Hotkey for week setup.

--- background.js
+++ background.js
@@ -17,9 +17,16 @@ function performAutoSetup() {
     chrome.tabs.executeScript(null, { code: 'performAutoSetup()' });
 }
 
+function performMultiDaySetup() {
+    chrome.tabs.executeScript(null, { code: 'performMultiDaySetup("current-week")' });
+}
+
 chrome.commands.onCommand.addListener(function(command) {
-    console.debug('automatic setup triggered by command (hotkey)');
-    if (command === 'perform-automatic-setup') {		
-        performAutoSetup();		
-    }		
+    if (command === 'perform-automatic-setup') {
+        console.debug('Automatic setup triggered by command (hotkey)');
+        performAutoSetup();
+    } else if (command === 'perform-current-week-setup') {
+        console.debug('Automatic current week setup triggered by command (hotkey)');
+        performMultiDaySetup();
+    }
 });
