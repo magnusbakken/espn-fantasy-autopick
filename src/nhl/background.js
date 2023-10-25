f2e77bac86adba50f81361f57bb9f99eac7bc8c2
Magnus Grindal Bakken <magnusbakken@gmail.com>
2023-10-25 15:55:03 +0000

    Fixed background pages.

--- src/nhl/background.js
+++ src/nhl/background.js
@@ -1,8 +1,10 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete' && tab.url.match(/fantasy.espn.com\/hockey\/team/)) {
-        chrome.pageAction.show(tabId);
-    } else {
-        chrome.pageAction.hide(tabId);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+        if (tab.url && tab.url.match(/fantasy.espn.com\/hockey\/team/)) {
+            chrome.pageAction.show(tabId);
+        } else {
+            chrome.pageAction.hide(tabId);
+        }
     }
 });
 
